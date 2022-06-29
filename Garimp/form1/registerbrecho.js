3ba6ba1955dde13e7fbe92916609165be8f640fd
Carlos Bessa <carlosbesssa@gmail.com>
2022-06-29 20:43:44 -0300

    Add files via upload
    Página de cadastro dos brechós.

---
+++ Garimp/form1/registerbrecho.js
@@ -0,0 +1,19 @@
+function goToStep2(){
+
+    let step1 = document.getElementById('step1')
+    step1.style.display = 'none'
+
+    let step2 = document.getElementById('step2')
+    step2.style.display = 'initial'
+
+}
+
+function backToStep2(){
+
+    let step1 = document.getElementById('step1')
+    step1.style.display = 'initial'
+
+    let step2 = document.getElementById('step2')
+    step2.style.display = 'none'
+
+}
